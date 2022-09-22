cb98ea341c942ed6384eab80dccb517d1d2c2392
Daniel Vieira <dann-vieira@hotmail.com>
2022-09-22 20:20:19 -0300

    feat: importação e exportação dos Archetypes

---
+++ src/Archetypes/index.ts
@@ -0,0 +1,13 @@
+import Mage from './Mage';
+import Archetype from './Archetype';
+import Necromancer from './Necromancer';
+import Warrior from './Warrior';
+import Ranger from './Ranger';
+ 
+export default Archetype;
+export { 
+  Mage,
+  Necromancer,
+  Warrior,
+  Ranger,
+};
